3f5c378e7d47e58a6a3e234f8af1edc18793ff06
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-03-21 21:43:36 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -62,7 +62,7 @@
 @endguest
 
 @auth
-@section
+@section('content')
 <div class="header bg-gradient-primary py-7 py-lg-8">
     <div class="container">
         <div class="header-body text-center mb-7">
