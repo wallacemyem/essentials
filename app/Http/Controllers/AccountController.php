9bf712e89a63e200240822f299102abd9d51103d
Wallace Aboiyar <wallacemyem@hotmail.com>
2020-03-21 19:59:30 +0100

    cc

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -38,7 +38,7 @@ class AccountController extends Controller
         $user = auth()->user()->username;
         $extension = $request->file('profile_image')->getClientOriginalExtension();
         $filenameToStore = $user.'.'.$extension;
-        $path = $request->file('profile_image')->storeAs('public/profile_image', $filenameToStore);
+        $path = $request->file('profile_image')->storeAs('https://brickng.com/essentials/profile_pix', $filenameToStore);
       }else{
         $filenameToStore = auth()->user()->profile_image;
       }
--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,6 +11,12 @@ use App\Http\Controllers\Controller;
 class TaskController extends Controller
 {
 
+
+    public function __construct()
+  {
+      $this->middleware('auth');
+  }
+
     /**
      * Store a newly created resource in storage.
      *
